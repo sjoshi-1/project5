7bad438622917c321459c59425bce9a51df8184f
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-20 19:42:12 -0400

    accessories page

--- accessories.php
+++ accessories.php
@@ -6,25 +6,25 @@
     <div class="row">
         <div class="column">
             <div class="card">
-                <img src="./images/shoes1.jpg" alt="Air Jordan" style="width:100%">
-                <h3>Air Jordan</h3>
+                <img src="./images/acc1.jpg" alt="Waterproof bag" style="width:100%">
+                <h3>Waterproof bag</h3>
                 <p class="price">$219.99</p>
                 
             </div>
         </div>
         <div class="column">
             <div class="card">
-                <img src="./images/shoes2.jpg" alt="Air Max fly" style="width:100%">
-                <h3>Tailored Jeans</h3>
-                <p class="price">$349.99</p>
+                <img src="./images/acc2.jpeg" alt="Leather bag" style="width:100%">
+                <h3>Leather bag</h3>
+                <p class="price">$159.99</p>
 
             </div>
         </div>
 
         <div class="column">
             <div class="card">
-                <img src="./images/shoes3.jpg" alt="Air Force Mid" style="width:100%">
-                <h3>Air Force Mid</h3>
+                <img src="./images/acc3.jpg" alt="Vintage Bag" style="width:100%">
+                <h3>Vintage Bag</h3>
                 <p class="price">$249.99</p>
                
             </div>
@@ -39,26 +39,26 @@
     <div class="row">
         <div class="column">
             <div class="card">
-                <img src="./images/cloth4.jpg" alt="Swoosh top" style="width:100%">
-                <h3>Swoosh top</h3>
+                <img src="./images/acc4.jpg" alt="Anklet socks" style="width:100%">
+                <h3>Anklet socks</h3>
                 <p class="price">$19.99</p>
                 
             </div>
         </div>
         <div class="column">
             <div class="card">
-                <img src="./images/cloth5.jpg" alt="Matching Set" style="width:100%">
-                <h3>Matching Set</h3>
-                <p class="price">$149.99</p>
+                <img src="./images/acc5.jpg" alt="3 pair socks" style="width:100%">
+                <h3>3 pair socks</h3>
+                <p class="price">$49.99</p>
 
             </div>
         </div>
 
         <div class="column">
             <div class="card">
-                <img src="./images/cloth6.jpg" alt="Legging" style="width:100%">
-                <h3>Legging</h3>
-                <p class="price">$80</p>
+                <img src="./images/acc6.jpg" alt="Long Socks" style="width:100%">
+                <h3>Long Socks</h3>
+                <p class="price">$20</p>
                
             </div>
         </div>
@@ -72,26 +72,26 @@
     <div class="row">
         <div class="column">
             <div class="card">
-                <img src="./images/cloth8.jpg" alt="Tempo Shorts" style="width:100%">
-                <h3>Tempo Shorts</h3>
+                <img src="./images/acc7.jpg" alt="Warm Gloves" style="width:100%">
+                <h3>Warm Gloves</h3>
                 <p class="price">$45</p>
                 
             </div>
         </div>
         <div class="column">
             <div class="card">
-                <img src="./images/cloth9.jpeg" alt="Hooded-jacket" style="width:100%">
-                <h3>Hooded Jacket</h3>
-                <p class="price">$125</p>
+                <img src="./images/acc8.jpg" alt="Baseball cap" style="width:100%">
+                <h3>Baseball cap</h3>
+                <p class="price">$25</p>
 
             </div>
         </div>
 
         <div class="column">
             <div class="card">
-                <img src="./images/cloth9.jpg" alt="Comfort-Trouser " style="width:100%">
-                <h3>Comfort Trouser</h3>
-                <p class="price">$105</p>
+                <img src="./images/acc9.jpg" alt="Flap-Cap " style="width:100%">
+                <h3>Flap-Cap</h3>
+                <p class="price">$25</p>
                
             </div>
         </div>
@@ -105,25 +105,25 @@
     <div class="row">
         <div class="column">
             <div class="card">
-                <img src="./images/cloth10.jpg" alt="Women Hoodie" style="width:100%">
-                <h3>Women Hoodie</h3>
-                <p class="price">$69.99</p>
+                <img src="./images/acc10.jpg" alt="Non-slip yoga mat" style="width:100%">
+                <h3>Non-slip yoga mat</h3>
+                <p class="price">$49.99</p>
                 
             </div>
         </div>
         <div class="column">
             <div class="card">
-                <img src="./images/cloth11.jpg" alt="Zip-up Hoodie" style="width:100%">
-                <h3>Zip up Hoodie</h3>
-                <p class="price">$175</p>
+                <img src="./images/acc11.jpg" alt="Filtered water bottle" style="width:100%">
+                <h3>Filtered water bottle</h3>
+                <p class="price">$75</p>
 
             </div>
         </div>
 
         <div class="column">
             <div class="card">
-                <img src="./images/cloth12.jpg" alt="Pullover Hoodie" style="width:100%">
-                <h3>Pullover Hoodie</h3>
+                <img src="./images/acc12.jpg" alt="Hot-water bottle" style="width:100%">
+                <h3>Hot-water bottle</h3>
                 <p class="price">$49.99</p>
                
             </div>
