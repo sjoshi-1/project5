04815133bc0fd81631c7aa1c9bf130bc0d0fc1b2
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-20 19:12:32 -0400

    clothing page

--- cloth.php
+++ cloth.php
@@ -2,3 +2,134 @@
  require('header.php');
 ?>
 <h1>Welcome to clothing page!!</h1>
+
+<div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth1.jpg" alt="basketball jersey" style="width:100%">
+                <h3>Basketball jersey</h3>
+                <p class="price">$109.99</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth2.jpg" alt="Gym-training" style="width:100%">
+                <h3>Gym-training</h3>
+                <p class="price">$149.99</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth3.jpg" alt="Plain tshirt" style="width:100%">
+                <h3>Plain t-shirt</h3>
+                <p class="price">$24.99</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
+
+    <!--2 row-->
+    <div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth4.jpg" alt="Swoosh top" style="width:100%">
+                <h3>Swoosh top</h3>
+                <p class="price">$19.99</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth5.jpg" alt="Matching Set" style="width:100%">
+                <h3>Matching Set</h3>
+                <p class="price">$149.99</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth6.jpg" alt="Legging" style="width:100%">
+                <h3>Legging</h3>
+                <p class="price">$80</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
+
+    <!--3 row-->
+    <div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth8.jpg" alt="Tempo Shorts" style="width:100%">
+                <h3>Tempo Shorts</h3>
+                <p class="price">$45</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth9.jpeg" alt="Hooded-jacket" style="width:100%">
+                <h3>Hooded Jacket</h3>
+                <p class="price">$125</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth9.jpg" alt="Comfort-Trouser " style="width:100%">
+                <h3>Comfort Trouser</h3>
+                <p class="price">$105</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
+
+    <!--4 row-->
+    <div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth10.jpg" alt="Women Hoodie" style="width:100%">
+                <h3>Women Hoodie</h3>
+                <p class="price">$69.99</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth11.jpg" alt="Zip-up Hoodie" style="width:100%">
+                <h3>Zip up Hoodie</h3>
+                <p class="price">$175</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/cloth12.jpg" alt="Pullover Hoodie" style="width:100%">
+                <h3>Pullover Hoodie</h3>
+                <p class="price">$49.99</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
