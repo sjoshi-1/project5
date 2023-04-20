64cf85c7fd6185955aa8a4288dd332718a95b447
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-20 18:42:00 -0400

    shoes page

--- about.php
+++ about.php
@@ -2,3 +2,8 @@
  require('header.php');
 ?>
 <h1>Welcome to about page!!</h1>
+<section class="about">
+<p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe rerum harum voluptate. Obcaecati labore tempore magnam quo perspiciatis vero reiciendis facere, cumque autem dignissimos, asperiores sed fugit repellendus ex.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe rerum harum voluptate. Obcaecati labore tempore magnam quo perspiciatis vero reiciendis facere, cumque autem dignissimos, asperiores sed fugit repellendus ex.
+</p>
+</section>
--- shoes.php
+++ shoes.php
@@ -2,3 +2,134 @@
  require('header.php');
 ?>
 <h1>Welcome to shoes page!!</h1>
+
+<div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes1.jpg" alt="Air Jordan" style="width:100%">
+                <h3>Air Jordan</h3>
+                <p class="price">$219.99</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes2.jpg" alt="Air Max fly" style="width:100%">
+                <h3>Tailored Jeans</h3>
+                <p class="price">$349.99</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes3.jpg" alt="Air Force Mid" style="width:100%">
+                <h3>Air Force Mid</h3>
+                <p class="price">$249.99</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
+
+    <!--2 row-->
+    <div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes4.jpg" alt="Air force high" style="width:100%">
+                <h3>Air Force -High</h3>
+                <p class="price">$179.99</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes6.jpg" alt="Air max" style="width:100%">
+                <h3>Air-max</h3>
+                <p class="price">$119.99</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes6.jpg" alt="Nike React revision" style="width:100%">
+                <h3>Nike react revision</h3>
+                <p class="price">$180</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
+
+    <!--3 row-->
+    <div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes7.jpg" alt="Nike BRSB" style="width:100%">
+                <h3>Nike BRSB</h3>
+                <p class="price">$120</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes8.jpg" alt="Nike Phantom GX" style="width:100%">
+                <h3>Nike phantom GX</h3>
+                <p class="price">$125</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes9.jpg" alt="Nike Waffle Debut " style="width:100%">
+                <h3>Nike Waffle Debut </h3>
+                <p class="price">$105</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
+
+    <!--4 row-->
+    <div id ="products">
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes10.jpg" alt="Running Shoes" style="width:100%">
+                <h3>Running Shoes</h3>
+                <p class="price">$129.99</p>
+                
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes11.jpg" alt="Air Max" style="width:100%">
+                <h3>Air Max</h3>
+                <p class="price">$175</p>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="./images/shoes12.jpg" alt="Nike TC" style="width:100%">
+                <h3>Nike TC</h3>
+                <p class="price">$109.99</p>
+               
+            </div>
+        </div>
+       
+        </div>
+
+    </div>
