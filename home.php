33ad487cf740d1282897a5d844f0a68d40a0fd6d
sjoshi-1 <200455953@student.georgianc.on.ca>
2023-04-22 18:53:20 -0400

    update files

--- home.php
+++ home.php
@@ -1,4 +1,14 @@
 <?php
  require('header.php');
+ // If they're not logged in, redirect them
+ session_start();
+ if (!$_SESSION['user']) {
+   $_SESSION['errors'][] = "You must log in.";
+   header("Location: login.php");
+   exit();
+ }
+
+ // Assign the user
+ $user = $_SESSION['user'];
 ?>
 <h1>Welcome to home page!!</h1>
--- signup.php
+++ signup.php
@@ -1,11 +1,12 @@
 <?php
-require('headerA.php');
 
+require('headerA.php');
 
 session_start();
 if (isset($_SESSION['user'])) {
   header("Location: home.php");
   exit();
+
 }
 
 $form_values = $_SESSION['form_values'] ?? null;
@@ -16,26 +17,26 @@ unset($_SESSION['form_values']);
 ?>
 
 <h3>Enjoy shopping by exploring more !!!!</h3>
-<form class="form">
+<form class="form" action="verify.php" method="post" novalidate>
 
 <!--name-->
 <label for="name"> Name: </label><br>
-<input type="text"  placeholder="Enter your name here"> <br>
+<input type="text" name="name" id="name" placeholder="Enter your name here" value= "<?= $form_values['name'] ?? null ?>" required> <br>
 
 
 
 <!--Email-->
 <label for="Email"> Email: </label><br>
-<input type="email"  placeholder="Enter your email here"><br> 
+<input type="email" name="email" id="email" placeholder="Enter your email here" required  value="<?= $form_values['email'] ?? null ?>"><br> 
   
 <!--password-->  
 <label for="password">Password : </label><br>
-<input type="password"  placeholder="Your password"><br>
+<input type="password" name="password"  id="password" placeholder="Your password" required><br>
 
 <!--Email-->
 <label for="rpass"> Retype Password: </label><br>
-<input type="password"  placeholder="Retype your password "><br> 
+<input type="password"  placeholder="Retype your password " name="password-confirmation"  id="password-confirmation" required><br> 
 
 <!--Log in page-->
-<a href="#" class="btn"> Register</a>
+<a  class="btn"> Register</a>
 </form>
